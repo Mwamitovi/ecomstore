81841b93d334438f4981c9a0d7926d66dadd7b4a
Martin Matovu <matovu.synergy@gmail.com>
2019-04-13 18:18:56 +0300

    Added the home() function to render index.html page

--- preview/views.py
+++ preview/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+# preview/views.py
+from django.shortcuts import render_to_response
 
-# Create your views here.
+
+def home(request):
+    return render_to_response("index.html")
