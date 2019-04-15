771cee52dbfca79637108ee67be0c360560742ce
Martin Matovu <matovu.synergy@gmail.com>
2019-04-15 12:15:21 +0300

    Updated STATIC_URL=(beginning part of the urlpattrn)

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -116,4 +116,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+# Set the static path to link to url pattern
+STATIC_URL = "/r'^static/"
