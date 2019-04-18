00ae1eba0515e233edd7531ba72e61e3b6c0306b
Martin Matovu <matovu.synergy@gmail.com>
2019-04-18 17:35:12 +0300

    Added show_category() to fetch our category page

--- catalog/views.py
+++ catalog/views.py
@@ -9,3 +9,14 @@ def index(request, template_name='catalog/index.html'):
     return render_to_response(
         template_name, locals(), RequestContext(request)
     )
+
+
+def show_category(request, category_slug, template_name='catalog/category.html'):
+    c = get_object_or_404(Category, slug=category_slug)
+    products = c.product_set.all()
+    page_title = c.name
+    meta_keywords = c.meta_keywords
+    meta_description = c.meta_description
+    return render_to_response(
+        template_name, locals(), RequestContext(request)
+    )
