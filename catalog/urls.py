e54378bf1133afe50277416396fc1ff93f8033ad
Martin Matovu <matovu.synergy@gmail.com>
2019-04-18 19:02:04 +0300

    Updated view functions within the urlpatterns

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,15 +4,15 @@ from catalog import views
 
 
 urlpatterns = [
-    url(r'^$', index,
-        {'template_name':'catalog/index.html'},
+    url(r'^$', views.index,
+        {'template_name': 'catalog/index.html'},
         name='catalog_home'
         ),
-    url(r'^category/(?P<category_slug>[-\w]/$)', show_category,
-        {'template_name':'catalog/category.html'},
+    url(r'^category/(?P<category_slug>[-\w]/$)', views.show_category,
+        {'template_name': 'catalog/category.html'},
         name='catalog_category'
         ),
-    url(r'^product/(?P<product_slug>[-\w]+)/$', show_product,
+    url(r'^product/(?P<product_slug>[-\w]+)/$', views.show_product,
         {'template_name':'catalog/product.html'},
         name='catalog_product'
         ),
