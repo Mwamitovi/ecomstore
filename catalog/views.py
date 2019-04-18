a1dd181f1c7518a0929f4e6aba1a7a60398c34af
Martin Matovu <matovu.synergy@gmail.com>
2019-04-18 17:33:56 +0300

    Added index() to fetch our index/home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# catalog/views.py
+from django.shortcuts import get_object_or_404, render_to_response
+from catalog.models import Category, Product
+from django.template import RequestContext
 
-# Create your views here.
+
+def index(request, template_name='catalog/index.html'):
+    page_title = 'Musical Instruments and Sheet Music for Musicians'
+    return render_to_response(
+        template_name, locals(), RequestContext(request)
+    )
