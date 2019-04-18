cd9213c01546fd069f0840ad841fa1f57319d95e
Martin Matovu <matovu.synergy@gmail.com>
2019-04-18 20:04:09 +0300

    Code refactor, removed hard-coded template_names from view functions

--- catalog/views.py
+++ catalog/views.py
@@ -4,14 +4,15 @@ from catalog.models import Category, Product
 from django.template import RequestContext
 
 
-def index(request, template_name='catalog/index.html'):
+def index(request, template_name):
     page_title = 'Musical Instruments and Sheet Music for Musicians'
+    context = RequestContext(request, )
     return render_to_response(
         template_name, locals(), RequestContext(request)
     )
 
 
-def show_category(request, category_slug, template_name='catalog/category.html'):
+def show_category(request, category_slug, template_name):
     c = get_object_or_404(Category, slug=category_slug)
     products = c.product_set.all()
     page_title = c.name
@@ -22,7 +23,7 @@ def show_category(request, category_slug, template_name='catalog/category.html')
     )
 
 
-def show_product(request, product_slug, template_name='catalog/product.html'):
+def show_product(request, product_slug, template_name):
     p = get_object_or_404(Product, slug=product_slug)
     categories = p.categories.filter(is_active=True)
     page_title = p.name
