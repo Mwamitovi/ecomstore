0ac0d0908b93d2a3aa06ae27d092457193b6e7aa
Martin Matovu <matovu.synergy@gmail.com>
2019-04-20 13:42:04 +0300

    Added _generate_cart_id() to create new cart_id

--- cart/cart.py
+++ cart/cart.py
@@ -3,6 +3,8 @@ from catalog.models import Product
 from cart.models import CartItem
 from ecomstore import settings
 
+import random
+
 
 CART_ID_SESSION_KEY = 'cart_id'
 
@@ -17,3 +19,12 @@ def _cart_id(request):
     if 'cart_id' in request.session:
         request.session['cart_id'] = _generate_cart_id()
     return request.session['cart_id']
+
+
+def _generate_cart_id():
+    cart_id = ''
+    characters = 'ABCDEFGHIJKLMNOPQRQSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()'
+    cart_id_length = 50
+    for y in range(cart_id_length):
+        cart_id += characters[random.randint(0, len(characters)-1)]
+    return cart_id
