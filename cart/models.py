3eeefd737f0ba5770eb23cd860612060928cc6e6
Martin Matovu <matovu.synergy@gmail.com>
2019-04-20 12:47:56 +0300

    Added "CartItem" model class

--- cart/models.py
+++ cart/models.py
@@ -1,1 +1,33 @@
 # cart/models.py
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
+from django.db import models
+from catalog.models import Product
+
+
+@python_2_unicode_compatible
+class CartItem(models.Model):
+    cart_id = models.CharField(max_length=50)
+    date_added = models.DateTimeField(auto_now_add=True)
+    quantity = models.IntegerField(default=1)
+    product = models.ForeignKey(Product, unique=False)
+
+    class Meta:
+        db_table = 'cart_items'
+        ordering = ['date_added']
+
+    def total(self):
+        return self.quantity * self.product.price
+
+    def name(self):
+        return self.product.name
+
+    def price(self):
+        return self.product.price
+
+    def get_absolute_url(self):
+        return self.product.get_absolute_url()
+
+    def augment_quantity(self):
+        self.quantity = self.quantity + int(self.quantity)
+        self.save()
