88c49cc31ce1bb1c177e8714015ca2725a72015c
Martin Matovu <matovu.synergy@gmail.com>
2019-04-22 11:48:56 +0300

    Added show_cart() to fetch the cart template

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,13 @@
+# cart/views.py
 from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from cart import cart
 
-# Create your views here.
+
+def show_cart(request, template_name):
+    cart_item_count = cart.cart_item_count(request)
+    page_title = 'Shopping Cart'
+    return render_to_response(
+        template_name, locals(), RequestContext(request)
+    )
