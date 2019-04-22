b88783442df31f494718fafd340c42292a2a9635
Martin Matovu <matovu.synergy@gmail.com>
2019-04-22 11:29:15 +0300

    Updated cart/urls.py to fetch show_cart()

--- cart/urls.py
+++ cart/urls.py
@@ -1,1 +1,10 @@
 # cart/urls.py
+from django.conf.urls import url
+from cart import views
+
+
+urlpatterns = [
+    url(r'^$', views.show_cart,
+        {'template_name': 'cart/cart.html'}, 'show_cart'
+        ),
+]
