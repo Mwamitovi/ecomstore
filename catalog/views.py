9b6f29eda82aa6433239eebec11437042771d766
Martin Matovu <matovu.synergy@gmail.com>
2019-04-24 15:25:18 +0300

    index(), & show_category() to return render(), not render_to_response

--- catalog/views.py
+++ catalog/views.py
@@ -1,20 +1,24 @@
 # catalog/views.py
-from django.shortcuts import get_object_or_404, render_to_response
+from django.shortcuts import get_object_or_404, render_to_response, render
 from catalog.models import Category, Product
 from django.template import RequestContext
 from django.core import urlresolvers
 from cart import cart
 from django.http import HttpResponseRedirect
 from catalog.forms import ProductAddToCartForm
+from utils import context_processors
 
 
 def index(request, template_name):
-    # category = get_object_or_404(Category, slug=category_slug)
     page_title = 'Musical Instruments and Sheet Music for Musicians'
-    # context =
-    return render_to_response(
-        # template_name, locals(), RequestContext(request, {'category': category})
-        template_name, locals(), RequestContext(request)
+    # return render_to_response(
+    #     template_name, locals(), RequestContext(request, [context_processors])
+    # )
+    return render(
+        request,
+        template_name,
+        {'page_title': page_title},
+        RequestContext(request, processors=[context_processors])
     )
 
 
@@ -24,8 +28,16 @@ def show_category(request, category_slug, template_name):
     page_title = c.name
     meta_keywords = c.meta_keywords
     meta_description = c.meta_description
-    return render_to_response(
-        template_name, locals(), RequestContext(request)
+    # return render_to_response(
+    #     template_name, locals(), RequestContext(request)
+    # )
+    return render(
+        request,
+        template_name,
+        {'products': products, 'page_title': page_title,
+         'meta_keywords': meta_keywords, 'meta_description': meta_description,
+        },
+        RequestContext(request, processors=[context_processors])
     )
 
 
@@ -54,7 +66,7 @@ def show_product(request, product_slug, template_name):
             return HttpResponseRedirect(url)
     else:
         # It's a GET, so create the unbound form,
-        # And notice the request is passes as a keyword arg (kwargs)
+        # And notice the request is passed as a keyword arg (kwargs)
         form = ProductAddToCartForm(request=request, label_suffix=':')
     # assign the hidden input the product slug
     form.fields['product_slug'].widget.attrs['value'] = product_slug
