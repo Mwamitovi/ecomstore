65268c6dd18daf2e294c4b26a159fa73c94954b9
Martin Matovu <matovu.synergy@gmail.com>
2019-04-24 20:29:16 +0300

    Updated show_cart() to return render(), not render_to_response()

--- cart/views.py
+++ cart/views.py
@@ -3,11 +3,18 @@ from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from cart import cart
+from utils import context_processors
 
 
 def show_cart(request, template_name):
     cart_item_count = cart.cart_item_count(request)
     page_title = 'Shopping Cart'
-    return render_to_response(
-        template_name, locals(), RequestContext(request)
+    # return render_to_response(
+    #     template_name, locals(), RequestContext(request)
+    # )
+    return render(
+        request,
+        template_name,
+        {'cart_item_count': cart_item_count, 'page_title': page_title, },
+        RequestContext(request, processors=[context_processors])
     )
