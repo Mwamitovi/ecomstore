195e88b5759ce016d757a602bd0088c9cee1ade9
Martin Matovu <matovu.synergy@gmail.com>
2019-04-30 14:22:04 +0300

    Updated show_cart() to update quantites and remove single items

--- cart/views.py
+++ cart/views.py
@@ -7,14 +7,22 @@ from utils import context_processors
 
 
 def show_cart(request, template_name):
-    cart_item_count = cart.cart_distinct_item_count(request)
+    """ view function for the page displaying the customer shopping cart,
+    and allows for the updating of quantities and removal of product instances
+    """
+    if request.method == 'POST':
+        postdata = request.POST.copy()
+        if postdata['submit'] == 'Remove':
+            cart.remove_from_cart(request)
+        if postdata['submit'] == 'Update':
+            cart.update_cart(request)
+    cart_items = cart.get_cart_items(request)
     page_title = 'Shopping Cart'
-    # return render_to_response(
-    #     template_name, locals(), RequestContext(request)
-    # )
+    cart_subtotal = cart.cart_subtotal(request)
+    # cart_item_count = cart.cart_distinct_item_count(request)
     return render(
         request,
         template_name,
-        {'cart_item_count': cart_item_count, 'page_title': page_title, },
+        locals(),
         RequestContext(request, processors=[context_processors])
     )
