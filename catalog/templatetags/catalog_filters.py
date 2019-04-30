4213d1038db0bb36cd4bdc8186067e515723cc55
Martin Matovu <matovu.synergy@gmail.com>
2019-04-30 17:07:45 +0300

    Added the currency() function, registered as a filter

--- catalog/templatetags/catalog_filters.py
+++ catalog/templatetags/catalog_filters.py
@@ -7,9 +7,5 @@ register = template.Library()
 
 @register.filter(name='currency')
 def currency(value):
-    try:
-        locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
-    except NameError:
-        locale.setlocale(locale.LC_ALL, '')
-    loc = locale.localeconv()
-    return locale.currency(value, loc['currency_symbol'], grouping=True)
+    return '$' + str(value)
+
