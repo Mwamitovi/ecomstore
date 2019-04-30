2a812b414274b1c610aff4052a186c5aac170197
Martin Matovu <matovu.synergy@gmail.com>
2019-04-30 17:32:31 +0300

    Addded "...middleware.FlatpagesFallbackMiddleware" to MIDDLEWARE

--- ecomstore/settings.py
+++ ecomstore/settings.py
@@ -47,6 +47,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # Added
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware'
 ]
 
 
