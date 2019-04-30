5908f471911b18335a989a0ae929dd11f6e0bfd8
Martin Matovu <matovu.synergy@gmail.com>
2019-04-30 14:55:39 +0300

    Removed 'active_categories' filed, it's now passed as an inclusion_tag

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -5,7 +5,6 @@ from ecomstore import settings
 
 def ecomstore(request):
     return {
-        'active_categories': Category.objects.filter(is_active=True),
         'site_name': settings.SITE_NAME,
         'meta_keywords': settings.META_KEYWORDS,
         'meta_description': settings.META_DESCRIPTION,
