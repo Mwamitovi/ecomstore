33010509c3f751b766207dd68453a8e7f730268a
Martin Matovu <matovu.synergy@gmail.com>
2019-05-01 13:55:29 +0300

    Updated show_cart,(), with logic to "get the checkout url", if Checkout

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,12 @@
 # cart/views.py
 from django.shortcuts import render
-from django.shortcuts import render_to_response
+# from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
 from cart import cart
 from utils import context_processors
+from checkout import checkout
+from ecomstore import settings
 
 
 def show_cart(request, template_name):
@@ -16,9 +19,14 @@ def show_cart(request, template_name):
             cart.remove_from_cart(request)
         if postdata['submit'] == 'Update':
             cart.update_cart(request)
+        if postdata['submit'] == 'Checkout':
+            checkout_url = checkout.get_checkout_url(request)
+            return HttpResponseRedirect(checkout_url)
     cart_items = cart.get_cart_items(request)
     page_title = 'Shopping Cart'
     cart_subtotal = cart.cart_subtotal(request)
+    # for Google Checkout button
+    merchant_id = settings.GOOGLE_CHECKOUT_MERCHANT_ID
 
     return render(
         request,
