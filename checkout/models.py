06e4ccf569606c64f56bbc495c1ebeef7bed0cd8
Martin Matovu <matovu.synergy@gmail.com>
2019-05-02 20:16:44 +0300

    Created "Order" model to contain info about the order being processed

--- checkout/models.py
+++ checkout/models.py
@@ -1,3 +1,101 @@
+# checkout/models.py
 from django.db import models
+from django import forms
+from django.contrib.auth.models import User
+from catalog.models import Product
+import decimal
 
-# Create your models here.
+
+class Order(models.Model):
+    # each individual status
+    SUBMITTED = 1
+    PROCESSED = 2
+    SHIPPED = 3
+    CANCELLED = 4
+
+    # set of possible order statuses
+    ORDER_STATUSES = (
+        (SUBMITTED, 'Submitted'),
+        (PROCESSED, 'Processed'),
+        (SHIPPED, 'Shipped'),
+        (CANCELLED, 'Cancelled'),
+    )
+
+    # order info
+    date = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=ORDER_STATUSES, default=SUBMITTED)
+    ip_address = models.IPAddressField()
+    last_updated = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, null=True)
+    transaction_id = models.CharField(max_length=20)
+
+    # contact info
+    email = models.EmailField(max_length=50)
+    phone = models.CharField(max_length=20)
+
+    # shipping information
+    shipping_name = models.CharField(max_length=50)
+    shipping_address_1 = models.CharField(max_length=50)
+    shipping_address_2 = models.CharField(max_length=50, blank=True)
+    shipping_city = models.CharField(max_length=50)
+    shipping_state = models.CharField(max_length=2)
+    shipping_country = models.CharField(max_length=50)
+    shipping_zip = models.CharField(max_length=10)
+
+    # billing information
+    billing_name = models.CharField(max_length=50)
+    billing_address_1 = models.CharField(max_length=50)
+    billing_address_2 = models.CharField(max_length=50, blank=True)
+    billing_city = models.CharField(max_length=50)
+    billing_state = models.CharField(max_length=2)
+    billing_country = models.CharField(max_length=50)
+    billing_zip = models.CharField(max_length=10)
+
+    def __str__(self):
+        return 'Order #' + str(self.pk)
+
+    @property
+    def total(self):
+        total = decimal.Decimal('0.00')
+        order_items = OrderItems.objects.filter(order=self)
+        for item in order_items:
+            total += item.total
+        return total
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
