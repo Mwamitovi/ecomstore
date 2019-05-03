4f9562211cfb6234b10ad88ebd8a20d21942c854
Martin Matovu <matovu.synergy@gmail.com>
2019-05-03 13:35:24 +0300

    Added process() to post a request, and return order number, and feedback

--- checkout/checkout.py
+++ checkout/checkout.py
@@ -14,3 +14,89 @@ def get_checkout_url(request):
 
     # use this for our own-site checkout
     return reverse('checkout')
+
+
+def process(request):
+    """ Takes a POST request containing valid order data;
+    pings the payment gateway with the billing information and
+    returns a Python dictionary with two entries: 'order_number' and 'message'
+    based on the success of the payment processing.
+
+    An unsuccessful billing will have an order_number of 0 and an error message,
+    and a successful billing with have an order number and an empty string message.
+    """
+
+    # Transaction results
+    _APPROVED = '1'
+    _DECLINED = '2'
+    _ERROR = '3'
+    _HELD_FOR_REVIEW = '4'
+    
+    postdata = request.POST.copy()
+    card_num = postdata.get('credit_card_number', '')
+    exp_month = postdata.get('credit_card_expire_month', '')
+    exp_year = postdata.get('credit_card_expire_year', '')
+    exp_date = exp_month + exp_year
+    cvv = postdata.get('credit_card_cvv', '')
+    amount = cart.cart_subtotal(request)
+    results = {}
+    response = authnet.do_auth_capture(
+        amount=amount,
+        card_num=card_num,
+        exp_date=exp_date,
+        card_cvv=cvv
+    )
+
+    if response[0] == _APPROVED:
+        transaction_id = response[6]
+        order = create_order(request, transaction_id)
+        results = {
+            'order_number': order.id,
+            'message': ''
+        }
+    if response[0] == _DECLINED:
+        results = {
+            'order_number': 0,
+            'message': 'There is a problem with your Credit Card.'
+        }
+    if response[0] == _ERROR or response[0] == _HELD_FOR_REVIEW:
+        results = {
+            'order_number': 0,
+            'message': 'Error processing your order.'
+        }
+    return results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
