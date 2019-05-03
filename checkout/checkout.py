aee3efbf2f00e83aea05156b5e4d2a9804ecc7ed
Martin Matovu <matovu.synergy@gmail.com>
2019-05-03 13:13:51 +0300

    Updated get_checkout_url() to return own-site checkout page

--- checkout/checkout.py
+++ checkout/checkout.py
@@ -1,9 +1,16 @@
 # checkout/checkout.py
-from checkout import google_checkout
+from django.core.urlresolvers import reverse
+import urllib
+from cart import cart
+from checkout.models import Order, OrderItem
+from checkout.forms import CheckoutForm
+from checkout import authnet
+from ecomstore import settings
 
 
+# noinspection PyUnusedLocal
 def get_checkout_url(request):
     """ returns the URL from the checkout module for cart """
 
-    # use this for Google Checkout API:
-    return google_checkout.get_checkout_url(request)
+    # use this for our own-site checkout
+    return reverse('checkout')
