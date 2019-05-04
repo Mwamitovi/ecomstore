c8dd9daf9cb130c8cd44f7d6dbb4041419ca3cf6
Martin Matovu <matovu.synergy@gmail.com>
2019-05-04 09:41:26 +0300

    Import "checkout.views" directly, into checkout.urls

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,16 +1,15 @@
 # checkout/urls.py
 from django.conf.urls import url
-# from checkout import views
+from checkout import views
 from ecomstore import settings
 
 
 urlpatterns = [
-    'checkout.views',
-    url(r'^$', 'show_checkout',
+    url(r'^$', views.show_checkout,
         {'template_name': 'checkout/checkout.html', 'SSL': settings.ENABLE_SSL},
         name='checkout'
         ),
-    url(r'^receipt/$', 'receipt',
+    url(r'^receipt/$', views.receipt,
         {'template_name': 'checkout/receipt.html', 'SSL': settings.ENABLE_SSL},
         name='checkout_receipt'
         ),
