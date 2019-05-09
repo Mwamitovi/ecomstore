370f905b80b500d56640f26cc8d0e10d42a20838
Martin Matovu <matovu.synergy@gmail.com>
2019-05-09 11:56:33 +0300

    Added the register() view function to render User form.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+# accounts/views.py
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.forms import UserCreationForm
+from django.template import RequestContext
+from django.core import urlresolvers
+from django.http import HttpResponseRedirect
+from utils import context_processors
 
-# Create your views here.
+
+def register(request, template_name):
+    if request.method == 'POST':
+        postdata = request.POST.copy()
+        form = UserCreationForm(postdata)
+        if form.is_valid():
+            form.save()
+            un = postdata.get('username', '')
+            pw = postdata.get('password', '')
+
+            from django.contrib.auth import login, authenticate
+            new_user = authenticate(username=un, password=pw)
+            if new_user and new_user.is_active:
+                login(request, new_user)
+                url = urlresolvers.reverse('accounts:my_account')
+                return HttpResponseRedirect(url)
+        else:
+            form = UserCreationForm()
+        page_title = 'User Registration'
+        return render(
+            request,
+            template_name,
+            locals(),
+            RequestContext(request, processors=[context_processors])
+        )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
