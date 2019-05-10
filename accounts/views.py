5531f1c0a703247a232cf729e496532582d3d86f
Martin Matovu <matovu.synergy@gmail.com>
2019-05-10 17:21:37 +0300

    Added the order_details() view function, to render content to the "order_detail" template

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from django.core import urlresolvers
+from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from utils import context_processors
 from checkout.models import Order, OrderItem
@@ -23,7 +23,7 @@ def register(request, template_name):
             new_user = authenticate(username=un, password=pw)
             if new_user and new_user.is_active:
                 login(request, new_user)
-                url = urlresolvers.reverse('accounts:my_account')
+                url = reverse('accounts:my_account')
                 return HttpResponseRedirect(url)
         else:
             form = UserCreationForm()
@@ -40,7 +40,7 @@ def register(request, template_name):
 def my_account(request, template_name):
     page_title = 'My Account'
     orders = Order.objects.filter(user=request.user)
-    name = request.user.user.username
+    name = request.user.username
     return render(
         request,
         template_name,
@@ -49,8 +49,17 @@ def my_account(request, template_name):
     )
 
 
-
-
+@login_required()
+def order_details(request, order_id, template_name):
+    order = get_object_or_404(Order, id=order_id, user=request.user)
+    page_title = 'Order Details for Order #' + order_id
+    order_items = OrderItem.objects.filter(order=order)
+    return render(
+        request,
+        template_name,
+        locals(),
+        RequestContext(request, processors=[context_processors])
+    )
 
 
 
