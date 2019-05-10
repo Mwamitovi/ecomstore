c2341909d9f39699c35cb720683ec88101a6c550
Martin Matovu <matovu.synergy@gmail.com>
2019-05-10 21:10:22 +0300

    Added to create_order(), logic to save profile info for future orders

--- checkout/checkout.py
+++ checkout/checkout.py
@@ -94,5 +94,11 @@ def create_order(request, transaction_id):
             oi.save()
         # all set, now clear the cart
         cart.empty_cart(request)
+
+        # save profile info for future orders
+        if request.user.is_authenticated():
+            from accounts import profile
+            profile.set(request)
+            
     # return the new order object
     return order
