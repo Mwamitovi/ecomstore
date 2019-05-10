d90fc7bb5d16514a85e7b87384330d867bcef98a
Martin Matovu <matovu.synergy@gmail.com>
2019-05-10 18:32:33 +0300

    Added "UserProfileForn" to capture customer contacts, billing & shipping info

---
+++ accounts/forms.py
@@ -0,0 +1,9 @@
+# accounts/forms/py
+from django import forms
+from accounts.models import UserProfile
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        exclude = ('user',)
