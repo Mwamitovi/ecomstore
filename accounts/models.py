decd9211dfea4f7fc158700552f0a6ba7664548a
Martin Matovu <matovu.synergy@gmail.com>
2019-05-10 18:02:52 +0300

    Added a "UserProfile()" model, with unique user profiles. Inherits from BaseOrderInfo()

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+# accounts/models.py
 from django.db import models
+from django.contrib.auth.models import User
+from checkout.models import BaseOrderInfo
 
-# Create your models here.
+
+class UserProfile(BaseOrderInfo):
+    user = models.ForeignKey(User, unique=True)
+
+    def __str__(self):
+        return 'User Profile for: ' + self.user.username
