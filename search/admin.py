3cdc95d59437b05ee27583a32f4b0b5b8ddad19c
Martin Matovu <matovu.synergy@gmail.com>
2019-05-19 18:32:49 +0300

    Created the "SearchTermAdmin" to render Model fields in the Admin-gui

--- search/admin.py
+++ search/admin.py
@@ -1,3 +1,12 @@
+# search/admin.py
 from django.contrib import admin
+from search.models import SearchTerm
 
-# Register your models here.
+
+class SearchTermAdmin(admin.ModelAdmin):
+    list_display = ('__str__', 'ip_address', 'search_date')
+    list_filter = ('ip_address', 'user', 'q')
+    exclude = ('user', )
+
+
+admin.site.register(SearchTerm, SearchTermAdmin)
