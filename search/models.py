42bdeea19f29db6df623feb549f6430d3cd6bd7d
Martin Matovu <matovu.synergy@gmail.com>
2019-05-19 18:31:51 +0300

    Created the "SearchTerm" model to store searched terms by clients.

--- search/models.py
+++ search/models.py
@@ -1,3 +1,13 @@
+# search/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class SearchTerm(models.Model):
+    q = models.CharField(max_length=50)
+    search_date = models.DateTimeField(auto_now_add=True)
+    ip_address = models.GenericIPAddressField()
+    user = models.ForeignKey(User, null=True)
+
+    def __str__(self):
+        return self.q
