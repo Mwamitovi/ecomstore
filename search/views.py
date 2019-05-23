2501894c1e5f4a18be3e0b3617e9a8d0b626a1f9
Martin Matovu <matovu.synergy@gmail.com>
2019-05-23 23:37:14 +0300

    Added the results() view function to render "search/results.html"

--- search/views.py
+++ search/views.py
@@ -1,3 +1,37 @@
+# search/views.py
 from django.shortcuts import render
+from django.template import RequestContext
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from search import search
+from ecomstore import settings
+from utils import context_processors
 
-# Create your views here.
+
+def results(request, template_name):
+    """ template for displaying settings.PRODUCTS_PER_PAGE
+        paginated product results
+    """
+    # get current search phrase
+    q = request.GET.get('q', '')
+    # get current page number. Set to 1, if missing or is invalid
+    try:
+        _page = int(request.GET.get('page', 1))
+    except ValueError:
+        _page = 1
+    # retrieve the matching products
+    matching = search.products(q).get('products')
+    # generate the paginator object
+    paginator = Paginator(matching, settings.PRODUCTS_PER_PAGE)
+    try:
+        _results = paginator.page(_page).object_list
+    except (InvalidPage, EmptyPage):
+        _results = paginator.page(1).object_list
+    # store the search
+    search.store(request, q)
+    page_title = 'Search Results for: ' + q
+    return render(
+        request,
+        template_name,
+        locals(),
+        RequestContext(request, processors=[context_processors])
+    )
