21c325c2735c5ab6be7602538e566e3e7d2fbc5e
Martin Matovu <matovu.synergy@gmail.com>
2019-05-29 12:16:44 +0300

    Added recommended_from_search() function, to return the common words from the stored search terms

--- stats/stats.py
+++ stats/stats.py
@@ -1,6 +1,8 @@
 # stats/stats.py
 import os
 import base64
+from search.models import SearchTerm
+from ecomstore.settings import PRODUCTS_PER_ROW
 
 
 def tracking_id(request):
@@ -12,3 +14,17 @@ def tracking_id(request):
     except KeyError:
         request.session['tracking_id'] = base64.b64encode(os.urandom(36))
         return request.session['tracking_id']
+
+
+def recommended_from_search(request):
+    """ get the common words from the stored searches """
+    common_words = frequent_search_words(request)
+    from search import search
+    matching = []
+    for word in common_words:
+        results = search.products(word).get('products', [])
+        for r in results:
+            if len(matching) < PRODUCTS_PER_ROW and r not in matching:
+                matching.append(r)
+    return matching
+
