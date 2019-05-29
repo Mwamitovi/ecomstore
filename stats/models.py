1c531d8496d38f14110286f7b2591afa65c4bef0
Martin Matovu <matovu.synergy@gmail.com>
2019-05-29 13:55:27 +0300

    Created PageView() model (an Abstract base class) for tracking pages viewed by a customer

--- stats/models.py
+++ stats/models.py
@@ -1,3 +1,15 @@
+# stats/models.py
 from django.db import models
+from django.contrib.auth.models import User
+from catalog.models import Product
 
-# Create your models here.
+
+class PageView(models.Model):
+    """ model class for tracking the pages that a customer views """
+    class Meta:
+        abstract = True
+
+    date = models.DateTimeField(auto_now=True)
+    ip_address = models.GenericIPAddressField()
+    user = models.ForeignKey(User, null=True)
+    tracking_id = models.CharField(max_length=50, default='')
