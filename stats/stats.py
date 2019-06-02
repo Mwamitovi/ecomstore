a9def77ed5f7528169c39c182cfd067e7beb05fb
Martin Matovu <matovu.synergy@gmail.com>
2019-06-02 21:31:22 +0300

    Updated recommended_from_views(), to filter "Product.active" based on related one-to-many "productview" class

--- stats/stats.py
+++ stats/stats.py
@@ -93,14 +93,14 @@ def recommended_from_views(request):
         t_ids = [v['tracking_id'] for v in products_viewed]
         # if there are other tracking-ids, get those products
         if t_ids:
-            all_viewed = Product.active.filter(products_viewed__tracking_id__in=t_ids)
+            all_viewed = Product.active.filter(productview__tracking_id__in=t_ids)
             # if there are other products, get them, but exclude
             # any products that the customer has already seen/viewed
             if all_viewed:
                 other_viewed = ProductView.objects.filter(
                     product__in=all_viewed).exclude(product__in=viewed)
                 if other_viewed:
-                    return Product.active.filter(products_viewed__in=other_viewed).distinct()
+                    return Product.active.filter(productview__in=other_viewed).distinct()
 
 
 def get_recently_viewed(request):
@@ -110,32 +110,3 @@ def get_recently_viewed(request):
         tracking_id=t_id).values('product_id').order_by('-date')[0:PRODUCTS_PER_ROW]
     product_ids = [v['product_id'] for v in views]
     return Product.active.filter(id__in=product_ids)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
