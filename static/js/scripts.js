7a9800325cf3dd813c68f1a583daaaf5b73f4ac8
Martin Matovu <matovu.synergy@gmail.com>
2019-06-07 17:53:40 +0300

    Updated the prepareDocument(), which calls other functions: - slideToggleReviewForm() which hides/shows review form, and - addProductReview() which makes a ajax request, and processes the response

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,46 @@
 
+function addProductReview(){
+    // build an object of review data to submit
+    var review = {
+        title: jQuery("#id_title").val(),
+        content: jQuery("#id_content").val(),
+        rating: jQuery("#id_rating").val(),
+        slug: jQuery("#id_slug").val()
+    };
+    // make a request, and process the response
+    jQuery.post("/review/product/add/", review,
+        function(response){
+            jQuery("#review_errors").empty();
+            // evaluate the 'success' parameter
+            if(response.success == "True"){
+                // disable the submit button, so as to prevent duplicates
+                jQuery("#submit_review").attr('disabled', 'disabled');
+                // if this is the first review, get rid of the 'No reviews' text
+                jQuery("#no_reviews").empty();
+                // then, add the new review to the reviews section
+                jQuery("#reviews").prepend(reponse.html).slideDown();
+                // and get the newly added review and style it with color
+                new_review = jQuery("#reviews").children(":first");
+                new_review.addClass("new_review");
+                // now, hide the review form
+                jQuery("#review_form").slideToggle();
+            } else {
+                // add the error text to the review_errors <div>
+                jQuery("#review_errors").append(response.html);
+            }
+        }, "json")
+}
+
+// toggles visibility of the "write review" link
+// and the product review form.
+function slideToggleReviewForm(){
+	jQuery("#review_form").slideToggle();
+	jQuery("#add_review").slideToggle();
+}
+
 // Main script file
 function prepareDocument(){
-    // code to prepare our page
+    // prepare the search bar
     jQuery("form#search").submit(function(){
         text = jQuery("#id_q").val();
         if(text == "" || text == "Search"){
@@ -11,6 +50,12 @@ function prepareDocument(){
             return false;
         }
     });
+    // prepare the product review form
+    jQuery("#submit_review").click(addProductReview);
+    jQuery("#review_form").addClass('hidden');
+    jQuery("#add_review").click(slideToggleReviewForm);
+    jQuery("#add_review").addClass('visible');
+    jQuery("#cancel_review").click(slideToggleReviewForm);
 }
 
 jQuery(document).ready(prepareDocument);
