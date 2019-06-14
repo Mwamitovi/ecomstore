20b403b2ec2f981c58ba526b3df0eb2fefe7f3bd
Martin Matovu <matovu.synergy@gmail.com>
2019-06-14 15:31:45 +0300

    Added the robots() to fetch the robots.txt for search engines

--- marketing/views.py
+++ marketing/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+# marketing/views.py
+from django.http import HttpResponse
+from ecomstore.settings import BASE_DIR
+import os
 
-# Create your views here.
+
+ROBOTS_PATH = os.path.join(BASE_DIR, 'marketing/robots.txt')
+
+
+def robots(request):
+    """ view for robots.txt file """
+    return HttpResponse(open(ROBOTS_PATH).read(), 'text/plain')
