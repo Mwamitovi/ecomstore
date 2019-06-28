aadd6378354802b174b31481d456c98c69f5b02c
Martin Matovu <matovu.synergy@gmail.com>
2019-06-29 00:34:58 +0300

    Define model Card(), with fields data, user and num. And property: display_number() and method: __str__

--- billing/models.py
+++ billing/models.py
@@ -1,3 +1,16 @@
+# billing/models.py
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Card(models.Model):
+    data = models.CharField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    num = models.CharField(max_length=4)
+
+    @property
+    def display_number(self):
+        return u'xxxx-xxxx-xxxx-' + str(self.num)
+
+    def __str__(self):
+        return str(self.user.username) + ' - ' + self.display_number
