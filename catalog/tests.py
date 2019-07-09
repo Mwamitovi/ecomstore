55240e0f027e6b28b00da36765a142633e253fea
Martin Matovu <matovu.synergy@gmail.com>
2019-07-09 15:12:13 +0300

    Refactor NewUserTestCase, add setUp() method and initiate the Client() here. (tests run almost 2x faster)

--- catalog/tests.py
+++ catalog/tests.py
@@ -6,10 +6,12 @@ import http
 
 class NewUserTestCase(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+
     def test_view_homepage(self):
-        client = Client()
         home_url = reverse('catalog:catalog_home')
-        response = client.get(home_url)
+        response = self.client.get(home_url)
         # check that we get a response
         self.failUnless(response)
         # check that status code of response is success
