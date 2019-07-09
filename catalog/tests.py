aa708645c1b00bcfe856c0d0f513ea460075d7a4
Martin Matovu <matovu.synergy@gmail.com>
2019-07-09 15:02:15 +0300

    Define NewUserTestCase, for anonymous user accessing the site home-page

--- catalog/tests.py
+++ catalog/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+# catalog/tests.py
+from django.test import TestCase, Client
+from django.urls import reverse
+import http
 
-# Create your tests here.
+
+class NewUserTestCase(TestCase):
+
+    def test_view_homepage(self):
+        client = Client()
+        home_url = reverse('catalog:catalog_home')
+        response = client.get(home_url)
+        # check that we get a response
+        self.failUnless(response)
+        # check that status code of response is success
+        # (http.HTTPStatus.OK = 200)
+        self.assertEqual(response.status_code, http.HTTPStatus.OK)
